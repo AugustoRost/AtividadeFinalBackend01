79e889433ea2f3a037fb98fc9a29c1d9e28e69f0
josé Augusto Rost da Silva <augustorost93@gmail.com>
2023-08-24 20:39:03 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express, { response } from "express";
+import { v4 as uuid } from "uuid";
+const app = express();
+app.use(express.json());
+
+const usuario = [];
+const mensagem = [];
+
+app.post("/cadastro", (req, res) => {
+  const emailJaUtilizado = usuario.find((usuario) => usuario.email === email);
+
+  if (emailJaUtilizado) {
+    return response.status(400).json({
+      mensage: "Email em uso!!!",
+    });
+  }
+});
+app.listen(8080, () => console.log("Servidor iniciado!!"));
