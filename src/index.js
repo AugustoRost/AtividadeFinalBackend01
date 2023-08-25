90e9d6687d19c05ab968fba3e4a2a7eda3e39d6e
josé Augusto Rost da Silva <augustorost93@gmail.com>
2023-08-24 22:25:47 -0300

    funcao criar usuario

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import express, { response } from "express";
-import { v4 as uuid } from "uuid";
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
 const app = express();
 app.use(express.json());
 
@@ -7,13 +7,26 @@ const usuario = [];
 const mensagem = [];
 
 app.post("/cadastro", (req, res) => {
+  const { nome, email, senha } = req.body;
   const emailJaUtilizado = usuario.find((usuario) => usuario.email === email);
 
   if (emailJaUtilizado) {
-    return response.status(400).json({
-      mensage: "Email em uso!!!",
+    return res.status(400).json({
+      message: "Email em uso!!!",
     });
   }
+  const novoUsuario = {
+    id: uuidv4(),
+    nome,
+    email,
+    senha,
+  };
+  usuario.push(novoUsuario);
+
+  res.status(201).json({
+    message: "Conta Criada",
+    usuario: novoUsuario,
+  });
 });
 
 app.listen(8080, () => console.log("Servidor iniciado!!"));
