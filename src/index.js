5cb673ac67a3a9278c1650a873d70477693f69ce
josé Augusto Rost da Silva <augustorost93@gmail.com>
2023-08-25 23:24:57 -0300

    login e validacoes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { v4 as uuidv4 } from "uuid";
 const app = express();
 app.use(express.json());
 
-const usuario = [];
-const mensagem = [];
+const usuarios = [];
+const mensagens = [];
 
 app.post("/cadastro", (req, res) => {
   const { nome, email, senha } = req.body;
-  const emailJaUtilizado = usuario.find((usuario) => usuario.email === email);
+  const emailJaUtilizado = usuarios.find(
+    (usuarios) => usuarios.email === email
+  );
 
   if (emailJaUtilizado) {
     return res.status(400).json({
@@ -21,12 +23,33 @@ app.post("/cadastro", (req, res) => {
     email,
     senha,
   };
-  usuario.push(novoUsuario);
+  usuarios.push(novoUsuario);
 
   res.status(201).json({
     message: "Conta Criada",
-    usuario: novoUsuario,
+    usuarios: novoUsuario,
   });
 });
 
+app.post("/login", (req, res) => {
+  const { senha, email } = req.body;
+
+  const usuarioEncontrado = usuarios.find((usuario) => usuario.email === email);
+
+  if (!usuarioEncontrado) {
+    return res.status(404).json({
+      message: "Senha ou Usuário inválidos!!",
+    });
+  }
+
+  if (usuarioEncontrado.senha !== senha) {
+    return res.status(404).json({
+      message: "Senha ou Usuário inválidos!!",
+    });
+  }
+
+  res.status(200).json({
+    message: "Login realizado com sucesso",
+  });
+});
 app.listen(8080, () => console.log("Servidor iniciado!!"));
